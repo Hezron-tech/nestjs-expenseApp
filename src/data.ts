31d3c4992876b4c72f8dfce0d6adce33754b89b9
Hezron-tech <kiprophez@gmail.com>
2023-04-03 11:24:48 +0300

    creating interface

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,19 @@
- const data ={
+export const data :Data ={
     report:[]
+ }
+
+ interface Data{
+    report:{
+        id:string;
+        source:string;
+        amount:number;
+        created_at:Date;
+        updated_at:Date;
+        type:ReportType
+    }[]
+ }
+
+ enum ReportType{
+    INCOME ="income",
+    EXPENSE ="expense"
  }
